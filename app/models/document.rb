26658cd43648bfc18acbd906be10a3af162cd378
lmbacelar <lmbacelar@gmail.com>
2012-04-06 13:13:26 +0100

    Added Document validations

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,5 @@
 class Document < ActiveRecord::Base
+  NameLengthRange = 1..25
+  validates :name,  presence: true, uniqueness: true, length: { in: NameLengthRange }
+  validates :title, presence: true, uniqueness: true
 end
