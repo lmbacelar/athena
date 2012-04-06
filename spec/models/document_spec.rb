8b18f0a9e9865c372e8bc22fac9e0467c70f42d6
lmbacelar <lmbacelar@gmail.com>
2012-04-06 13:13:09 +0100

    Added Document specs (validation)

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Document do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:doc) { FactoryGirl.create(:document) }
+
+  describe 'name validation' do
+    it 'fails when empty' do
+      FactoryGirl.build(:document, name: nil).should_not be_valid
+    end
+
+    it 'fails when duplicate' do
+      FactoryGirl.build(:document, name: doc.name).should_not be_valid
+    end
+
+    it 'fails when too small' do
+      char_count = (Document::NameLengthRange.first - 1)
+      FactoryGirl.build(:document, name: "-" * char_count).should_not be_valid
+    end
+
+    it 'fails when too large' do
+      char_count = (Document::NameLengthRange.last + 1)
+      FactoryGirl.build(:document, name: "-" * char_count).should_not be_valid
+    end
+  end
+
+  describe 'title validation' do
+    it 'fails when empty' do
+      FactoryGirl.build(:document, title: nil).should_not be_valid
+    end
+    it 'fails when duplicate' do
+      FactoryGirl.build(:document, title: doc.title).should_not be_valid
+    end
+  end
 end
