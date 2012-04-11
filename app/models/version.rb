0dc24147afea0cd1d9bead26eb7babd562bcc851
lmbacelar <lmbacelar@gmail.com>
2012-04-11 01:33:26 +0100

    Added timeline validations. Added state method.

--- app/models/version.rb
+++ app/models/version.rb
@@ -5,9 +5,23 @@ class Version < ActiveRecord::Base
   validates :document, presence: true
   validates :authored_date, presence: true
   validates :authored_date, date: { before_or_equal_to: Proc.new { Time.zone.now } }
+  validates :checked_date, date: { after_or_equal_to: :authored_date, allow_nil: true }
+  validates :approved_date, date: { after_or_equal_to: :checked_date, allow_nil: true }
+  validates :withdrawn_date, date: { after_or_equal_to: :authored_date, allow_nil: true }
 
   # Associations
   belongs_to :document, touch: true
 
   # Methods
+  def state
+    if withdrawn_date
+      :withdrawn
+    elsif approved_date
+      :approved
+    elsif checked_date
+      :checked
+    else
+      :draft
+    end
+  end
 end
