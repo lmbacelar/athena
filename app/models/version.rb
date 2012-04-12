f86635143823b3ba701fe95ae06e70fd60600e02
lmbacelar <lmbacelar@gmail.com>
2012-04-12 23:11:44 +0100

    Added callbacks to autoincrement number, create initial state_change record. Refactoring

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,26 +1,44 @@
 class Version < ActiveRecord::Base
-  # Validations
+
+  # # # # # Callbacks
+  before_validation :increment_number
+  after_create      :create_state_change
+
+  # # # # # Attr Protection
+  # TODO: change to attr_accessible when accessible
+  #       attrs are defined (content ...)
+  attr_protected :number, :state
+
+  # # # # # Validations
   validates :number, presence: true, uniqueness: { scope: :document_id }
   validates :document, presence: true
 
-  # Associations
+  # # # # # Associations
   belongs_to :document, touch: true
   has_many :state_changes, dependent: :destroy
 
-  # State Machine Setup
+  # # # # # State Machine Setup
   state_machine initial: :initiated do
     after_transition :add_state_change
 
-    event :check do ( transition :initiated => :draft ) end
-    event :publish do ( transition [:draft, :withdrawn] => :published ) end
-    event :withdraw do ( transition :published => :withdrawn ) end
-    event :discard do ( transition [:initiated, :draft] => :discarded ) end
+    event :check    do ( transition :initiated           => :draft     ) end
+    event :publish  do ( transition [:draft, :withdrawn] => :published ) end
+    event :withdraw do ( transition :published           => :withdrawn ) end
+    event :discard  do ( transition [:initiated, :draft] => :discarded ) end
   end
 
 private
+  def create_state_change
+    self.state_changes.create state: human_state_name, user: 'User'
+  end
+
   def add_state_change(transition)
-    self.state_changes.create user: 'User',
-                              state: transition.human_to_name,
-                              event: transition.human_event
+    self.state_changes.create state: transition.human_to_name, user: 'User'
+  end
+
+  def increment_number
+    if document
+      self.number = 1 + (document.versions.maximum(:number) || 0)
+    end
   end
 end
