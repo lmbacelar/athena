54899fd0f1524eea132283d6f435d5a4ef45296c
lmbacelar <lmbacelar@gmail.com>
2012-04-13 03:10:47 +0100

    Added specs for StateChange model

--- spec/models/state_change_spec.rb
+++ spec/models/state_change_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe StateChange do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations:' do
+    it { should validate_presence_of :user }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :version }
+  end
+
+  describe 'associations:' do
+    it { should belong_to :version }
+  end
 end
