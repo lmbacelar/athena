3435cd0e0f75eb933fbf024968c4c4eed202278d
lmbacelar <lmbacelar@gmail.com>
2012-04-14 20:27:11 +0100

    Added user name on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-User.create email: 'user@i-dea.tk', password: 'secret', password_confirmation: 'secret'
-l1 = Category.create name: 'Manual da Qualidade', acronym: 'MQ', level: 1
-mq = l1.documents.create name: '01', title: 'Manual da Qualidade', description: 'Manual da Qualidade do Laboratorio de Calibracoes da TAP Portugal'
+User.create email: 'lmbacelar@gmail.com',
+            name: 'Luis Bacelar',
+            password: 'secret',
+            password_confirmation: 'secret'
+l1 = Category.create  name: 'Manual da Qualidade',
+                      acronym: 'MQ',
+                      level: 1
+mq = l1.documents.create name: '01',
+                         title: 'Manual da Qualidade',
+                         description: 'Manual da Qualidade do Laboratorio de Calibracoes da TAP Portugal'
 v1 = mq.versions.create
