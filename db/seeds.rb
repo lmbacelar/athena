2054c7866d5af8a47987b796c63919170eb87bd1
lmbacelar <lmbacelar@gmail.com>
2012-04-14 18:17:56 +0100

    Added mnimal seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create email: 'user@i-dea.tk', password: 'secret', password_confirmation: 'secret'
+l1 = Category.create name: 'Manual da Qualidade', acronym: 'MQ', level: 1
+mq = l1.documents.create name: '01', title: 'Manual da Qualidade', description: 'Manual da Qualidade do Laboratorio de Calibracoes da TAP Portugal'
+v1 = mq.versions.create
