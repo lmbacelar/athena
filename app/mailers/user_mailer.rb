917601f81101e0da5fdadb209fe4fe74d4566ea3
lmbacelar <lmbacelar@gmail.com>
2012-04-15 18:05:07 +0100

    updated :from parameter

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'athena@i-dea.tk'
+  default from: 'lmbacelar@tabkey.net'
 
   # Subject is set in I18n file at config/locales/en.yml
   #   en.user_mailer.password_reset.subject
