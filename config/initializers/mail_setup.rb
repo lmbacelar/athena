719dc7aa74ed3913a5620bf979f9f565180fa45a
lmbacelar <lmbacelar@gmail.com>
2012-04-15 18:04:03 +0100

    Mail setup to use gmail smtp

---
+++ config/initializers/mail_setup.rb
@@ -0,0 +1,11 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "tabkey.net",
+  :user_name            => "lmbacelar@tabkey.net",
+  :password             => "2inhOFFF",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options[:locale] = I18n.locale
