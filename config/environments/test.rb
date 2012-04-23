122f40d9fa6dc8c22776a47ad0762214ac45e350
lmbacelar <lmbacelar@gmail.com>
2012-04-23 16:12:33 +0100

    Configuring mailer on test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,8 @@ Athena::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Sets up mailer
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
 end
