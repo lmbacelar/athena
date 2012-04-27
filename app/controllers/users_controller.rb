bf8543d308d25f58e2543795c8fef633d616e1df
lmbacelar <lmbacelar@gmail.com>
2012-04-28 00:41:03 +0100

    Refactored code. Added show, edit, update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  respond_to :html, :json
+
+  def show
+    @user = User.find(params[:id])
+    respond_with @user
+  end
 
   def new
     @user = User.new
@@ -11,6 +17,19 @@ class UsersController < ApplicationController
       redirect_to root_url, notice: t('flash.notice.user_activation_email_sent')
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to root_url, notice: "#{User.model_name.human} #{ t('flash.notice.update')}"
+    else
+      render action: 'edit'
     end
   end
 
